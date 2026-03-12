caa92b0b3baee335728cadb0adaf772ecf6167ad
Rahma Ghoumia <rgghoumia@gmail.com>
2026-03-12 11:36:41 +0100

    fix: conftest compatible Windows et Linux

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
 import sys
 import os
 
-sys.path.insert(0, "C:/Program Files/Webots/lib/controller/python")
-os.environ["WEBOTS_HOME"] = "C:/Program Files/Webots"
-os.add_dll_directory("C:/Program Files/Webots/lib/controller")
-os.environ["PATH"] = "C:/Program Files/Webots/lib/controller;" + os.environ["PATH"]
+# Chemins Webots selon le système d'exploitation
+if os.name == 'nt':  # Windows
+    WEBOTS_HOME = "C:/Program Files/Webots"
+    sys.path.insert(0, f"{WEBOTS_HOME}/lib/controller/python")
+    os.environ["WEBOTS_HOME"] = WEBOTS_HOME
+    os.add_dll_directory(f"{WEBOTS_HOME}/lib/controller")
+    os.environ["PATH"] = f"{WEBOTS_HOME}/lib/controller;" + os.environ["PATH"]
+else:  # Linux (GitHub Actions)
+    WEBOTS_HOME = "/usr/local/webots"
+    sys.path.insert(0, f"{WEBOTS_HOME}/lib/controller/python")
+    os.environ["WEBOTS_HOME"] = WEBOTS_HOME
